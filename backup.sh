489cd6b15d8982087045fb3f92702600eb4547f9
SACHIN KUMAR YADAV <116642788+SACHINKUMAR1728@users.noreply.github.com>
2023-06-02 02:00:06 +0530

    Update backup.sh
    deleted useless line of code

--- backup.sh
+++ backup.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
 
+#directory to be backed up
 source_dir="/home/waze/Desktop"
 
+#directory where backup need to be stored
 backup_dir="/home/waze"
 
+#create name of backup file 
 backup_filename="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
 
+#following code will create backup using tar command
 tar -czvf "${backup_dir}/${backup_filename}" "${source_dir}"
 
-
-if[ $? -eq 0 ]; then
-	echo "Backup created succesfully"
-else
-	echo "Backup creation failed."
-fi
